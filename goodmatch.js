def0e7f55ef45e633a2f9e4c8208db88f5bf7de9
Live Tembiso <g21T8571@ict.ru.ac.za>
2021-09-08 17:26:22 +0200

    FindMatch working

--- goodmatch.js
+++ goodmatch.js
@@ -1,5 +1,33 @@
-var players = prompt("Please enter players: ");
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+   
+  readline.question('Enter players: ', players => {
+    findMatch(players);  
+    readline.close();
+  })
 
-function findMatch(){
-    print(players)
+function findMatch(players){
+    let playersArray = (players.replace(/\s+/g, '')).split('');
+    console.log(playersArray);
+    let freq =  new Map();
+    for(let i=0; i < playersArray.length; i++){
+        if(freq.has(playersArray[i])){
+            freq.set(playersArray[i],parseInt(freq.get(playersArray[i]))+1);
+        } else{
+            freq.set(playersArray[i],1);
+        }
+
+        // console.log(playersArray[i]);
+        //Player1 matches Player2
+    }
+
+    let valueArray = Array.from(freq.values());
+
+    // for(let a = 0; a < freq.length; a++){
+    //     console.log(freq[a]);
+    // }
+    console.log(valueArray);
+    console.log(freq.values());
 }
